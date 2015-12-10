8e35d3280d30d139ab311be00aaeefaa4f0a3603
cmccomb <ccmcc2012@gmail.com>
2015-12-10 11:23:56 -0500

    Added basic wrapper for CISAT api.

---
+++ wrapper/cisat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class CISAT(object):
+
+    def build(problem_statement, grammar, design_problem):
+        # Write the cmakelists.txt file
+        # Build the thing
+        # Save the location of the compiled file
+
+    def new_config(option_dict):
+        # Writes a configuraiton file
+
+    def run():
+        # Runs cisat based on config file
+
+    
