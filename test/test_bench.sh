45b6864fc8c83eb1b8eee56f0f74912fecd3973d
Chris McComb <ccmcc2012@gmail.com>
2021-05-04 13:10:09 -0400

    Update test_bench.sh

--- test/test_bench.sh
+++ test/test_bench.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Test the benchmarking functionality
-../build/bin/cisat --bench --input example_bench.txt --output output_bench.txt --verbose
+../build/bin/cisat --bench --input example_bench.txt --output output_bench.txt
